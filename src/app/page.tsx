39763226fd309f07f4c49695af7da6877ae80c85
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-19 15:47:34 +0900

    Design: 전체적인 레이아웃 크기 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import AccountComponent from "@/features/Account/components/AccountComponent";
 
 export default function Home() {
   return (
-    <div>
-      <AccountComponent />
+    <div className="w-full">
+      <div className="fixed left-0 top-[180px] hidden laptop:block">
+        <AccountComponent />
+      </div>
     </div>
   );
 }
