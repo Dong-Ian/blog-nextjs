81dc438508a5026d19a5e6bf8edfe095ba9fcca3
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-21 09:41:16 +0900

    Design: 메인 페이지 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import AccountComponent from "@/features/Account/components/AccountComponent";
+import PinnedPostList from "@/features/PostList/components/PinnedPostList";
+import RecentPostList from "@/features/PostList/components/RecentPostList";
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <div className="fixed left-0 top-[180px] hidden laptop:block">
+    <div className="flex justify-center">
+      <div className="hidden lg:fixed lg:left-0 lg:top-40 lg:block">
         <AccountComponent />
+      </div>
+
+      <div className="mt-[100px] flex w-full max-w-[760px] flex-col gap-6 px-4">
+        <PinnedPostList page={1} size={5} />
+        <RecentPostList page={1} size={5} />
       </div>
     </div>
   );
