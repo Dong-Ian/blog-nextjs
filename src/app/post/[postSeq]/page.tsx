b0787e28e303d40243f8424e6163068cf98bcbef
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-21 13:24:29 +0900

    Design: Editor z-index 조정

--- src/app/post/[postSeq]/page.tsx
+++ src/app/post/[postSeq]/page.tsx
@@ -1,11 +1,21 @@
+import EditorMarkdown from "@/components/atoms/EditorMarkdown";
+import Typography from "@/components/atoms/Typography";
+import getPost from "@/features/Post/services/getPost.service";
+import { PostInterface } from "@/features/PostList/types/PostList.type";
+
 export default async function Post({
   params,
 }: {
   params: { postSeq: string };
 }) {
+  const postSeq = (await params).postSeq;
+  const post: PostInterface = await getPost({ postSeq: postSeq });
+
   return (
-    <div>
-      <p>게시글 아이디: {params.postSeq}</p>
+    <div className="mt-[100px]">
+      <Typography.P1>{post.category}</Typography.P1>
+      <Typography.Head2>{post.postTitle}</Typography.Head2>
+      <EditorMarkdown content={post.postContents} />
     </div>
   );
 }
