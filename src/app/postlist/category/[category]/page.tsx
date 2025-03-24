2e7581a4ed437395869ffc7a163eeff5ec9e54c6
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-24 15:33:53 +0900

    Feat: Pagination 구현

--- src/app/postlist/category/[category]/page.tsx
+++ src/app/postlist/category/[category]/page.tsx
@@ -1,28 +1,58 @@
+"use client";
 import Typography from "@/components/atoms/Typography";
+import Pagination from "@/components/molecules/Pagination";
 import PostItem from "@/features/PostList/components/PostItem";
 import { getCategoryPostList } from "@/features/PostList/services/getPostList.service";
 import { PostInterface } from "@/features/PostList/types/PostList.type";
+import { useRouter, useSearchParams } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-export default async function CategoryPostList({
+export default function CategoryPostList({
   params,
 }: {
-  params: { category: string };
+  params: Promise<{ category: string }>;
 }) {
-  const category = decodeURIComponent((await params).category);
-  const postList = await getCategoryPostList({
-    page: 1,
-    size: 5,
-    category: category,
-  });
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pageParam = searchParams.get("page");
+  const page = pageParam ? Number(pageParam) : 1;
+
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const { category } = use(params);
+  const decodedCategory = decodeURIComponent(category);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getCategoryPostList({
+        page,
+        size: 5,
+        category: decodedCategory,
+      });
+      setPosts(res.postList);
+      setTotalPages(res.postCount);
+    })();
+  }, [page]);
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    router.push(`/postlist/category/${category}?page=${selected + 1}`);
+  };
 
   return (
     <div className="m-auto flex w-[90%] max-w-screen-md flex-col justify-center">
       <Typography.SubTitle1 className="ml-[30px] text-gray-500">
-        {category}
+        {decodedCategory}
       </Typography.SubTitle1>
-      {postList.postList.map((post: PostInterface) => (
+      {posts.map((post: PostInterface) => (
         <PostItem key={post.postSeq} post={post} />
       ))}
+      <Pagination
+        postCount={totalPages}
+        perPage={5}
+        currentPage={page}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
--- src/app/postlist/page.tsx
+++ src/app/postlist/page.tsx
@@ -1,19 +1,50 @@
+"use client";
 import Typography from "@/components/atoms/Typography";
+import Pagination from "@/components/molecules/Pagination";
 import PostItem from "@/features/PostList/components/PostItem";
 import { getRecentPostList } from "@/features/PostList/services/getPostList.service";
 import { PostInterface } from "@/features/PostList/types/PostList.type";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function PostList() {
-  const postList = await getRecentPostList({ page: 1, size: 5 });
+export default function PostList() {
+  const router = useRouter();
 
+  const searchParams = useSearchParams();
+  const pageParam = searchParams.get("page");
+  const page = pageParam ? Number(pageParam) : 1;
+
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getRecentPostList({
+        page,
+        size: 5,
+      });
+      setPosts(res.unpinnedPostList);
+      setTotalPages(res.postCount);
+    })();
+  }, [page]);
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    router.push(`/postlist?page=${selected + 1}`);
+  };
   return (
     <div className="m-auto flex w-[90%] max-w-screen-md flex-col justify-center">
       <Typography.SubTitle1 className="ml-[30px] text-gray-500">
         최신글
       </Typography.SubTitle1>
-      {postList.pinnedPostList.map((post: PostInterface) => (
+      {posts.map((post: PostInterface) => (
         <PostItem key={post.postSeq} post={post} />
       ))}
+      <Pagination
+        postCount={totalPages}
+        perPage={5}
+        currentPage={page}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
