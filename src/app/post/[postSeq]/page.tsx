89e97dc643bb9a46b18d730bfd2df2c15dc1c37f
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-27 11:34:14 +0900

    Feat: ScrollToBottomButton 추가

--- src/app/post/[postSeq]/page.tsx
+++ src/app/post/[postSeq]/page.tsx
@@ -5,6 +5,7 @@ import PostAction from "@/features/Post/components/PostAction";
 import PostComment from "@/features/Post/components/PostComment";
 import PostContents from "@/features/Post/components/PostContents";
 import PostHeader from "@/features/Post/components/PostHeader";
+import ScrollToBottomButton from "@/features/Post/components/ScrollToBottomButton";
 import getPost from "@/features/Post/services/getPost.service";
 import { PostInterface } from "@/features/Post/type/Post.type";
 
@@ -18,6 +19,7 @@ export default async function Post({
 
   return (
     <div className="flex w-full justify-center">
+      <ScrollToBottomButton />
       <div className="hidden lg:fixed lg:left-0 lg:top-40 lg:block">
         <AccountComponent />
       </div>
---
+++ src/features/Post/components/ScrollToBottomButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Button from "@/components/atoms/Button";
+
+const ScrollToBottomButton = () => {
+  const handleScrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <Button.Default
+      type="button"
+      className="fixed bottom-10 right-4 z-50 flex size-[60px] items-center justify-center rounded-full bg-white shadow-md"
+      onClick={handleScrollToBottom}
+    >
+      <i className="bi bi-arrow-down text-[28px]"></i>
+    </Button.Default>
+  );
+};
+
+export default ScrollToBottomButton;
