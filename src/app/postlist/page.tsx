b0c68d7c4138c9980a99e92fee73c9f4d3be1b22
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-28 16:10:03 +0900

    Feat: pagination 변경

--- src/app/postlist/page.tsx
+++ src/app/postlist/page.tsx
@@ -1,50 +1,19 @@
-"use client";
-import Typography from "@/components/atoms/Typography";
-import Pagination from "@/components/molecules/Pagination";
-import PostItem from "@/features/PostList/components/PostItem";
 import { getRecentPostList } from "@/features/PostList/services/getPostList.service";
-import { PostInterface } from "@/features/PostList/types/PostList.type";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import PostListClient from "./PostListClient";
 
-export default function PostList() {
-  const router = useRouter();
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { page?: string };
+}) {
+  const page = Number((await searchParams).page ?? 1);
+  const res = await getRecentPostList({ page, size: 5 });
 
-  const searchParams = useSearchParams();
-  const pageParam = searchParams.get("page");
-  const page = pageParam ? Number(pageParam) : 1;
-
-  const [posts, setPosts] = useState<PostInterface[]>([]);
-  const [totalPages, setTotalPages] = useState(1);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getRecentPostList({
-        page,
-        size: 5,
-      });
-      setPosts(res.unpinnedPostList);
-      setTotalPages(res.postCount);
-    })();
-  }, [page]);
-
-  const handlePageChange = ({ selected }: { selected: number }) => {
-    router.push(`/postlist?page=${selected + 1}`);
-  };
   return (
-    <div className="m-auto flex w-[90%] max-w-screen-md flex-col justify-center">
-      <Typography.SubTitle1 className="ml-[30px] text-gray-500">
-        최신글
-      </Typography.SubTitle1>
-      {posts.map((post: PostInterface) => (
-        <PostItem key={post.postSeq} post={post} />
-      ))}
-      <Pagination
-        postCount={totalPages}
-        perPage={5}
-        currentPage={page}
-        onPageChange={handlePageChange}
-      />
-    </div>
+    <PostListClient
+      posts={res.unpinnedPostList}
+      totalPages={res.postCount}
+      currentPage={page}
+    />
   );
 }
--- src/components/molecules/Pagination.tsx
+++ src/components/molecules/Pagination.tsx
@@ -1,5 +1,7 @@
 "use client";
-import ReactPaginate from "react-paginate";
+import { cn } from "@/lib/utils";
+import React from "react";
+import Button from "../atoms/Button";
 
 interface PaginationProps {
   postCount: number;
@@ -8,32 +10,86 @@ interface PaginationProps {
   onPageChange: (selectedItem: { selected: number }) => void;
 }
 
-const Pagination = ({
+const Pagination: React.FC<PaginationProps> = ({
   postCount,
   perPage,
   currentPage,
   onPageChange,
-}: PaginationProps) => {
-  const pageCount = Math.ceil(postCount / perPage);
-  const style =
-    "w-[40px] py-2 border rounded-lg cursor-pointer flex justify-center";
+}) => {
+  const totalPage = Math.ceil(postCount / perPage);
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+
+  const handleChange = (page: number) => {
+    if (page >= 1 && page <= totalPage) {
+      onPageChange({ selected: page - 1 });
+    }
+  };
+  const maxPageToShow = 5;
+  let startPage = Math.max(1, currentPage - 2);
+  let endPage = startPage + maxPageToShow - 1;
+
+  if (currentPage <= 3) {
+    // 처음 3페이지는 무조건 1부터 시작
+    startPage = 1;
+    endPage = maxPageToShow;
+  }
+
+  if (endPage > totalPage) {
+    endPage = totalPage;
+    startPage = Math.max(1, endPage - maxPageToShow + 1);
+  }
+
+  const generatePagination = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => {
+      const pageNumber = startPage + i;
+      return (
+        <Button.Default
+          color="Main"
+          key={pageNumber}
+          className={cn(
+            pageNumber === currentPage && "bg-black text-white",
+            "hover:bg-black hover:text-white"
+          )}
+          onClick={() => handleChange(pageNumber)}
+        >
+          {pageNumber}
+        </Button.Default>
+      );
+    }
+  );
   return (
-    <div className="py-[30px]">
-      <ReactPaginate
-        previousLabel={"<"}
-        nextLabel={">"}
-        breakLabel={"..."}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        forcePage={currentPage - 1}
-        onPageChange={onPageChange}
-        containerClassName="flex gap-2 justify-center mt-5"
-        pageClassName={style}
-        activeClassName="bg-black text-white"
-        previousClassName={style}
-        nextClassName={style}
-      />
+    <div className="flex flex-wrap justify-center gap-2 py-6">
+      <Button.Default
+        disabled={currentPage === 1}
+        onClick={() => handleChange(1)}
+        className="hover:bg-black hover:text-white"
+      >
+        처음
+      </Button.Default>
+      <Button.Default
+        disabled={currentPage === 1}
+        onClick={() => handleChange(prevPage)}
+        className="hover:bg-black hover:text-white"
+      >
+        이전
+      </Button.Default>
+      {generatePagination}
+      <Button.Default
+        disabled={currentPage === totalPage}
+        onClick={() => handleChange(nextPage)}
+        className="hover:bg-black hover:text-white"
+      >
+        다음
+      </Button.Default>
+      <Button.Default
+        disabled={currentPage === totalPage}
+        onClick={() => handleChange(totalPage)}
+        className="hover:bg-black hover:text-white"
+      >
+        마지막
+      </Button.Default>
     </div>
   );
 };
