f32f9e99d3c4136f892dd9895fb75b78eca0aa87
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-28 15:22:29 +0900

    Feat: Loading 페이지에 스피너 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,33 @@
-const loading = () => {
-  return <div>loading....</div>;
+"use client";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const Loading = () => {
+  const pathname = usePathname();
+  const hasHeader =
+    pathname === "/" ||
+    pathname === "/post" ||
+    pathname.startsWith("/post/") ||
+    pathname === "/postlist" ||
+    pathname.startsWith("/postlist/");
+
+  return (
+    <div
+      className={cn(
+        "flex h-screen items-center justify-center",
+        hasHeader && "mt-[-100px]"
+      )}
+    >
+      <div
+        className="size-[70px] animate-spin rounded-full 
+                   border-8 border-gray-200 border-t-black"
+        style={{
+          animationDuration: "1s",
+          animationTimingFunction: "ease-in-out",
+        }}
+      />
+    </div>
+  );
 };
 
-export default loading;
+export default Loading;
