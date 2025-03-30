84c9be94571963aa342a57c2519b2ee427f1a7b8
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-30 19:30:40 +0900

    Desing: 홈 skeleton ui 디자인 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import SkeletonPostList from "@/components/molecules/SkeletonPostList";
 const Loading = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex w-full max-w-[760px] flex-col gap-6 px-4">
+      <div className="mt-[30px] flex w-full max-w-[760px] flex-col gap-6 px-4">
         <SkeletonPostList />
         <SkeletonPostList />
         <SkeletonPostList />
--- src/components/molecules/SkeletonPostList.tsx
+++ src/components/molecules/SkeletonPostList.tsx
@@ -2,25 +2,11 @@ import Skeleton from "../atoms/Skeleton";
 
 const SkeletonPostList = () => {
   return (
-    <div className="ml-6 mt-10 flex w-full max-w-[800px] flex-col gap-2">
-      <Skeleton width="100px" height="16px" rounded="rounded-lg" />
-      <Skeleton
-        width="290px"
-        height="22px"
-        rounded="rounded-lg"
-        className="mb-5"
-      />
-      <Skeleton width="100%" height="15px" rounded="rounded-lg" />
-      <Skeleton width="100%" height="15px" rounded="rounded-lg" />
-      <Skeleton width="100%" height="15px" rounded="rounded-lg" />
-      <Skeleton width="100%" height="15px" rounded="rounded-lg" />
-
-      <Skeleton
-        width="200px"
-        height="12px"
-        rounded="rounded-lg"
-        className="mt-3"
-      />
+    <div className="ml-6 mt-[50px] flex w-full max-w-[800px] flex-col gap-2 ">
+      <Skeleton width={100} height={16} rounded="rounded-lg" />
+      <Skeleton width={290} height={22} rounded="rounded-lg" className="mb-5" />
+      <Skeleton width="100%" height={100} rounded="rounded-lg" />
+      <Skeleton width={300} height={12} rounded="rounded-lg" className="mt-3" />
     </div>
   );
 };
