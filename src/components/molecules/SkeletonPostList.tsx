8b320dfae4a172967433c1dac9e1aa9d8ffc664d
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-31 18:43:23 +0900

    Desing: 메인페이지 스켈레톤 디자인 변경

--- src/components/molecules/SkeletonPostList.tsx
+++ src/components/molecules/SkeletonPostList.tsx
@@ -2,7 +2,7 @@ import Skeleton from "../atoms/Skeleton";
 
 const SkeletonPostList = () => {
   return (
-    <div className="ml-6 mt-[50px] flex w-full max-w-[800px] flex-col gap-2 ">
+    <div className="mt-[50px] flex w-full max-w-[800px] flex-col gap-2 ">
       <Skeleton width={100} height={16} rounded="rounded-lg" />
       <Skeleton width={290} height={22} rounded="rounded-lg" className="mb-5" />
       <Skeleton width="100%" height={100} rounded="rounded-lg" />
