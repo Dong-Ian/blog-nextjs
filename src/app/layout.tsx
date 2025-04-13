4f7d880145bcb3a61790ad7db4dedb147ca8a938
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-13 23:06:59 +0900

    Feat: 메타데이터 분기처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,40 @@
-"use client";
 import QueryClientProvider from "@/app/queryClientProvider";
-import Header from "@/components/molecules/Header";
-import { cn } from "@/lib/utils";
-import AppInitializer from "@/shared/components/AppInitializer";
+import AppInitializer from "@/shared/utils/AppInitializer";
+import HeaderWrapper from "@/shared/utils/HeaderWrapper";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { usePathname } from "next/navigation";
-
+import { Metadata } from "next";
 import "../styles/global.css";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const env = process.env.NEXT_PUBLIC_ENV;
+
+  return {
+    title: env === "IAN" ? "Archive" : "Tech Blog",
+    description:
+      env === "IAN"
+        ? "최신 프론트엔드 트렌드를 바탕으로 효율적인 개발 방법과 실무 경험을 나눕니다."
+        : "동현이의 개발 블로그",
+  };
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const showHeader =
-    pathname === "/" ||
-    pathname === "/post" ||
-    pathname.startsWith("/post/") ||
-    pathname === "/postlist" ||
-    pathname.startsWith("/postlist/") ||
-    pathname === "/admin";
+  const env = process.env.NEXT_PUBLIC_BLOG_ID;
+  const faviconPath =
+    env === "IAN" ? "/favicon/minsook.ico" : "/favicon/dong.ico";
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={faviconPath} />
+      </head>
       <body suppressHydrationWarning>
         <QueryClientProvider>
-          {showHeader && <Header />}
-          <div
-            className={cn(
-              "flex w-full items-center justify-center",
-              showHeader && "mt-[100px]"
-            )}
-          >
+          <HeaderWrapper />
+          <div className="flex w-full items-center justify-center">
             <AppInitializer />
             {children}
             <div id="portal" />
---
+++ src/shared/utils/HeaderWrapper.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Header from "@/components/molecules/Header";
+import { usePathname } from "next/navigation";
+
+export default function HeaderWrapper() {
+  const pathname = usePathname();
+  const showHeader =
+    pathname === "/" ||
+    pathname === "/post" ||
+    pathname.startsWith("/post/") ||
+    pathname === "/postlist" ||
+    pathname.startsWith("/postlist/") ||
+    pathname === "/admin";
+
+  return showHeader ? (
+    <div className="mb-[100px]">
+      <Header />
+    </div>
+  ) : null;
+}
