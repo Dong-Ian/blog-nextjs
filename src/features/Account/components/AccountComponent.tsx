8b95898f4aae4e8fb50dab89f3863565b395db55
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-18 11:13:34 +0900

    Fix: AccountComponent 렌더링 시점 변경
    userInfo가 로딩되지 않았을 때 깨지는 문제 해결

--- src/features/Account/components/AccountComponent.tsx
+++ src/features/Account/components/AccountComponent.tsx
@@ -10,38 +10,40 @@ import SocialInfo from "./SocialInfo";
 const AccountComponent = () => {
   const { userInfo } = useUserStore();
 
-  return (
-    <div className="flex w-[400px] flex-col items-center justify-center p-[30px]">
-      <div className="relative mb-[30px] size-[150px] overflow-hidden rounded-full">
-        {userInfo.images.profileImage && (
-          <Image
-            alt="profile"
-            src={userInfo.images.profileImage}
-            className="object-cover"
-            fill
-          />
-        )}
-      </div>
-      <div className="mb-[20px] flex flex-col items-center gap-1">
-        <Typography.SubTitle1 className="text-[30px] font-bold">
-          {userInfo.userName}
-        </Typography.SubTitle1>
-        <Typography.P2>{userInfo.memo}</Typography.P2>
-      </div>
+  if (userInfo) {
+    return (
+      <div className="flex w-[400px] flex-col items-center justify-center p-[30px]">
+        <div className="relative mb-[30px] size-[150px] overflow-hidden rounded-full">
+          {userInfo.images.profileImage && (
+            <Image
+              alt="profile"
+              src={userInfo.images.profileImage}
+              className="object-cover"
+              fill
+            />
+          )}
+        </div>
+        <div className="mb-[20px] flex flex-col items-center gap-1">
+          <Typography.SubTitle1 className="text-[30px] font-bold">
+            {userInfo.userName}
+          </Typography.SubTitle1>
+          <Typography.P2>{userInfo.memo}</Typography.P2>
+        </div>
 
-      <ProfileDetail title="이메일 정보">
-        <Typography.P3>{userInfo.userEmail}</Typography.P3>
-      </ProfileDetail>
-      <Divider width={95} />
-      <ProfileDetail title="소셜 정보">
-        <SocialInfo userInfo={userInfo} />
-      </ProfileDetail>
-      <Divider width={95} />
-      <ProfileDetail title="링크">
-        <PersonalUrl url={userInfo.personalUrl} />
-      </ProfileDetail>
-    </div>
-  );
+        <ProfileDetail title="이메일 정보">
+          <Typography.P3>{userInfo.userEmail}</Typography.P3>
+        </ProfileDetail>
+        <Divider width={95} />
+        <ProfileDetail title="소셜 정보">
+          <SocialInfo userInfo={userInfo} />
+        </ProfileDetail>
+        <Divider width={95} />
+        <ProfileDetail title="링크">
+          <PersonalUrl url={userInfo.personalUrl} />
+        </ProfileDetail>
+      </div>
+    );
+  }
 };
 
 export default AccountComponent;
