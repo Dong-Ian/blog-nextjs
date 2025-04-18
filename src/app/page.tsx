418bcb743380cdfb54246190bc5a2a79d905fd5d
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-18 20:29:21 +0900

    Fix: account 함수 ssr에서 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Divider from "@/components/atoms/Divider";
 import AccountComponent from "@/features/Account/components/AccountComponent";
-import getAccount from "@/features/Account/services/getAccount.service";
 import MainPageCategory from "@/features/Category/components/MainPageCategory";
 import getCategoryList from "@/features/Category/services/getCategoryList.service";
 import PinnedPostList from "@/features/PostList/components/PinnedPostList";
 import RecentPostList from "@/features/PostList/components/RecentPostList";
 
 export default async function Home() {
-  const userInfo = await getAccount();
   const categoryList = await getCategoryList();
   return (
     <div>
-      <title>{userInfo.title}</title>
       <div className="flex justify-center">
         <div className="hidden lg:fixed lg:left-0 lg:top-40 lg:block">
           <AccountComponent />
--- src/features/Account/services/getAccount.service.ts
+++ src/features/Account/services/getAccount.service.ts
@@ -11,7 +11,7 @@
 //   );
 
 //   const res = await result.json();
-//   console.log(res);
+
 //   return res.profileResult;
 // }
 
