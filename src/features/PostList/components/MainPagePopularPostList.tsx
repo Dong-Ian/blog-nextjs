3b5166d14039605a315323486312a94d63200318
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-03 18:28:23 +0900

    Feat: 인기글 제목 글자 길이 제한

--- src/features/PostList/components/MainPagePopularPostList.tsx
+++ src/features/PostList/components/MainPagePopularPostList.tsx
@@ -12,18 +12,24 @@ const MainPagePopularPostList = ({
   return (
     <div className="mr-[45px] w-[350px] border-l-2 px-10 ">
       <Typography.P2 className="mb-6 font-bold">인기글</Typography.P2>
-      {postList.map((post, index) => (
-        <Link
-          href={`/post/${post.postSeq}`}
-          key={index}
-          className="flex h-8 items-center justify-between transition-all hover:text-sky-400"
-        >
-          <div className="flex items-center justify-start gap-2">
-            <Typography.P2>{post.title}</Typography.P2>
-          </div>
-          <i className="bi bi-arrow-right"></i>
-        </Link>
-      ))}
+      {postList.map((post, index) => {
+        const title = post.title.slice(0, 22);
+        return (
+          <Link
+            href={`/post/${post.postSeq}`}
+            key={index}
+            className="flex h-8 items-center justify-between transition-all hover:text-sky-400"
+          >
+            <div className="flex items-center justify-start gap-2">
+              <Typography.P2>
+                {title}
+                {post.title.length > 22 && " ..."}
+              </Typography.P2>
+            </div>
+            <i className="bi bi-arrow-right"></i>
+          </Link>
+        );
+      })}
     </div>
   );
 };
